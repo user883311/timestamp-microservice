52000506f3942c08b98d6d06934b55a065b66c3d
user883311 <tarik.chardon@gmail.com>
2018-03-08 23:34:58 +0800

    1st GET response back to local host.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const os = require("os");
+console.log(`Total memory: ${os.totalmem()}`);
+console.log(`Free memory: ${os.freemem()}`);
+
+const fs = require("fs");
+
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
@@ -6,10 +17,12 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/dateValues", function(req, res, next){
-    console.log("get worked...");
+app.get("/dateValues", function (req, res, next) {
+    
+    console.log("GET worked...");
+    res.end("Hello world!");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("yes it works...");
 });
