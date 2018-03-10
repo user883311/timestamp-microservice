d93e08e2f21d4a529704a3d8ff4f9c408c57acff
user883311 <tarik.chardon@gmail.com>
2018-03-10 16:55:56 +0800

    Working API.

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# timestamp-microservice
+# Timestamp microservice
 ## a freecodecamp project
-
-
-API Basejump: Timestamp microservice
 
 User stories:
 1) I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016)
-2) If it does, it returns both the Unix timestamp and the natural language form of that date.
+2) If it does, it returns both the Unix *UTC* timestamp and the natural language form of that date.
 3) If it does not contain a date or Unix timestamp, it returns null for those properties.
 
 Example usage:
@@ -15,3 +12,7 @@ https://timestamp-ms.herokuapp.com/1450137600
 
 Example output:
 { "unix": 1450137600, "natural": "December 15, 2015" } 
+
+Built using Node.js' HTTP module. 
+
+Source: https://www.freecodecamp.org/challenges/timestamp-microservice
--- app.js
+++ app.js
@@ -1,28 +1,89 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-
-// console.log(__dirname);
-// console.log(__filename);
-
-const os = require("os");
-console.log(`Total memory: ${os.totalmem()}`);
-console.log(`Free memory: ${os.freemem()}`);
-
-const fs = require("fs");
-
-const bodyParser = require("body-parser");
-const express = require("express");
-const cors = require("cors");
-
-var app = module.exports = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get("/dateValues", function (req, res, next) {
-    
-    console.log("GET worked...");
-    res.end("Hello world!");
+const server = http.createServer((req, res) => {
+    var result = new Object();
+    if (/^\/.+$/.test(req.url)) {
+        console.log("There is a parameter in this request.");
+        res.write(returnTimeStampJSON(req.url));
+        res.json()
+        res.end();
+    }
+    else {
+        result.unix = null;
+        result.natural = null;
+        res.end();
+    }
 });
 
-app.listen(3000, function () {
-    console.log("yes it works...");
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function returnTimeStampJSON(param) {
+    let result = new Object();
+    param = param.slice(1);
+
+    // unix date parameter
+    let unixRegex = new RegExp();
+    unixRegex = /^\d+$/;
+    // natural format date parameter
+    let natRegex = new RegExp();
+    natRegex = /(January|February|March|April|May|June|July|August|September|October|November|December)( |\%20)?\d{1,2},?( |%20) ?\d{2,4}/;
+
+    if (unixRegex.test(param)) {
+        result.unix = param;
+        result.natural = unixToNatural(param);
+    }
+    else if (natRegex.test(param)) {
+        param = param.replace(/\%20/g, " ");
+        result.unix = naturalToUnix(param);
+        result.natural = param;
+    }
+    else {
+        result.unix = null;
+        result.natural = null;
+    }
+    return JSON.stringify(result);
+}
+
+function unixToNatural(unixTimeStamp) {
+    /* This function takes in a number (unix timestamp)
+    and returns a natural date in the following format: 
+    "December 15, 2015". */
+    const mmmmMonths = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let nat = new Date(unixTimeStamp * 1000); // for milliseconds
+    console.log(nat);
+    let result = mmmmMonths[nat.getMonth()];
+    result += " ";
+    result += nat.getDate();
+    result += ", ";
+    result += nat.getFullYear();
+    return result;
+}
+
+function naturalToUnix(naturalDateString) {
+    /* This function takes in a natural date in the 
+    following format: "December 15, 2015" and returns 
+    a number (unix timestamp) : 1450137600. */
+    let dateObj = new Date(naturalDateString);
+    let offset = dateObj.getTimezoneOffset();
+    offset *= 60; // for seconds
+    let unix = dateObj.getTime() / 1000; // for seconds
+    unix -= offset;
+    return unix;
+}
